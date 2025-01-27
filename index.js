8a427529b3876b6d51455fe6903c3729f4703f68
sornsomavatey <2024026sorn@aupp.edu.kh>
2025-01-27 16:09:29 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('I am Learning Cloud Developing'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 80
